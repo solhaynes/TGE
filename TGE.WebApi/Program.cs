de59bd1b7541940f6364366d74e9441aca7f66fa
Solomon Haynes <solomonhaynes22@gmail.com>
2024-06-12 18:58:17 -0400

    Made changes to Program.cs file

--- TGE.WebApi/Program.cs
+++ TGE.WebApi/Program.cs
@@ -1,6 +1,27 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TGE.Data;
+using TGE.Data.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Add connection string and DbContext setup
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<UserEntity>(options => 
+{
+    // Password configuration, optional
+    options.Password.RequiredLength = 4;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireNonAlphanumeric = false;
+})
+    .AddRoles<IdentityRole<int>>() // Enable Roles, optional
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
