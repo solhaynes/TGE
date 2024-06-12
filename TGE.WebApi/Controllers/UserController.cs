5b09e65534824274526e0928b8fc42674febe744
Solomon Haynes <solomonhaynes22@gmail.com>
2024-06-12 19:45:08 -0400

    set up controller and service injection

---
+++ TGE.WebApi/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using TGE.Services.User;
+
+namespace MyApp.Namespace
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+    }
+}
--- TGE.WebApi/Program.cs
+++ TGE.WebApi/Program.cs
@@ -13,36 +13,4 @@ builder.Services.AddScoped<IUserService, UserService>();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
-builder.Services.AddDefaultIdentity<UserEntity>(options => 
-{
-    // Password configuration, optional
-    options.Password.RequiredLength = 4;
-    options.Password.RequireUppercase = false;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireDigit = false;
-    options.Password.RequireNonAlphanumeric = false;
-})
-    .AddRoles<IdentityRole<int>>() // Enable Roles, optional
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+builder.Services.AddD
